f403ba2d25974df1c190c5b33ae34d81de531d32
Gregierr <grzegorz12346@op.pl>
2023-04-05 00:38:54 +0200

    search video

--- src/Form/SearchType.php
+++ src/Form/SearchType.php
@@ -2,7 +2,17 @@
 
 namespace App\Form;
 
-class SearchType
-{
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
 
+class SearchType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('query', TextType::class)
+            ->add('submit', SubmitType::class);
+    }
 }
